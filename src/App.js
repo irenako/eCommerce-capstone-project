f11b00c5436b0cd2f61d11c23c1a554870a36f1d
irenako <108225581+irenako@users.noreply.github.com>
2022-11-15 19:15:15 +0200

    Video 99

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/routes/home/home.component";
-import Navigation from "./components/routes/home/navigation/navigation.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 import { Fragment } from "react";
 
-import { ReactComponent as CrwnLogo } from "../../../../assets/crown.svg";
+import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
 
 import "./navigation.styles.scss";
 
@@ -15,6 +15,9 @@ const Navigation = () => {
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            Sign in
           </Link>
         </div>
       </div>
